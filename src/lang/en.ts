a998886c7cfa9a417db2873f7ff25f988d7daffb
Eric Thomas <bluebird6922@gmail.com>
2022-07-18 09:18:50 +0300

    add config, lang

---
+++ src/lang/en.ts
@@ -0,0 +1,11 @@
+const Lang = {
+  entryToken: 'entry token',
+  entryCoinName: 'PiXL Entry Coin: 1st Edition | Ultra-Rare',
+  entryCoinAlt:
+    'Entry Coin for PiXL RPG\nCreated by Lex (@LexUnity)\n\nHOW IT WORKS\nIn order to gain access to the PiXL game world you need to have at least one of these coins in your wallet. You can only use one, so having more than one will not help you, share/sell your extra coins if you have a few. \n\nThe aim of PiXL is to be a play-to-earn game that’s accessible to all. Once a week, (Fridays at 5p PST) the top offers and random offers will be accepted and receive a coin. There will be tons of opportunities to get an entry coin. For the latest - follow @PiXLtez \n\n----\n\nPlay PiXL: PiXL.xyz\n\nEntry Coin concept adapted from Beets (@wideawakebeets)\n\nCoin design by: Elin Hohler \n',
+  noEntryCoinFound: 'No entry coin found in wallet',
+  connectYourWallet: 'Please connect your wallet',
+  pixltezMinted: 'PiXLtez has been successfully minted',
+  pixltezMintFailed: 'Failed to mint PiXLtez',
+};
+export default Lang;
