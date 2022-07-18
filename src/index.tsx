989aa1c3341c9452a5ddc2661b30e8de029cc87c
Eric Thomas <bluebird6922@gmail.com>
2022-07-18 09:18:22 +0300

    update packages

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'animate.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Providers from './Providers';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Providers>
+      <App />
+    </Providers>
   </React.StrictMode>
 );
 
